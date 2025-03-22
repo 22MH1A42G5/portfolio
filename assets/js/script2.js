6efb711dd721f98d12a2ae2d0d4de433460a162d
ram <kamparapusriram@gmail.com>
2025-03-22 16:46:01 +0530

    initial commit

---
+++ assets/js/script2.js
@@ -0,0 +1,33 @@
+(function() {
+  emailjs.init({ publicKey: 'fA6Ccq2YQm2IB1kAC' }); // Correct Public Key
+})();
+
+document.getElementById('contactUs-Form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const form = {
+      name: document.getElementById('name').value,
+      email: document.getElementById('email').value,
+      message: document.getElementById('comment').value
+  };
+
+  emailjs.send(
+      'service_exzbqim', // Correct Service ID
+      'template_7qdnder', // Correct Template ID
+      {
+          name: form.name,
+          email: form.email,
+          message: form.message
+      }
+  ).then(
+      function(response) {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('Thank you! Your message has been sent.');
+          document.getElementById('contactUs-Form').reset();
+      },
+      function(error) {
+          console.error('FAILED...', error);
+          alert('Something went wrong. Check the console for details.');
+      }
+  );
+});
